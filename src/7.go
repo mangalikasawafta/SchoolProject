8dbce7e00ee28496a46b456330e1e663ec63c1a4
mangalikasawafta <mangalikasawafta@gmail.com>
2025-03-13 10:41:18 +0000

    Add 7.go

---
+++ src/7.go
@@ -0,0 +1,14 @@
+
+// Package declaration
+package main
+
+// Import statement
+import "fmt"
+
+func main() {
+	// Declare a variable and initialize it to 42
+	var answer int = 42
+
+	// Print the value of the variable
+	fmt.Println("The answer is", answer)
+}
