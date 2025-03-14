6eba9eb69267ecfa14bae0942cdd386ee8cf0ae4
mangalikasawafta <mangalikasawafta@gmail.com>
2025-03-14 18:34:06 +0000

    Add 8.go

---
+++ src/8.go
@@ -0,0 +1,17 @@
+
+func main() {
+	// Create a new array with 5 elements
+	numbers := []int{1, 2, 3, 4, 5}
+	
+	// Get the first element of the array
+	firstNum := numbers[0]
+	
+	// Print the first element
+	fmt.Println(firstNum)
+	
+	// Update the value of the first element to 10
+	numbers[0] = 10
+	
+	// Print the updated array
+	fmt.Println(numbers)
+}
